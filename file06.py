597a868943ebdb8025265b67185705de347bf571
Navruzq <qarshiyevnavruz7777@gmail.com>
2022-11-10 02:59:09 +0500

    6

--- file06.py
+++ file06.py
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+
+    l=data.split('\n')
+    a=[]
+    for i in l:
+        a.append(len(str(i)))
+    return a
 # Read data from file
+data = open('txt_file/data06.txt').read()
+print(main(data))  
