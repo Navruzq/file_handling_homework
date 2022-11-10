b0f07b9e61a1ef86b82e692f59942f6a37144902
Navruzq <qarshiyevnavruz7777@gmail.com>
2022-11-10 23:37:16 +0500

    7

--- file07.py
+++ file07.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         int: return answer
     """
-    
+    i=0
+    s=0 
+    for i in data:
+        if i.isdigit():
+           s=s+int(i)
+    return s
+
+
+data = open('txt_file/data07.txt').read()
+print(main(data))   
 # Read data from file
